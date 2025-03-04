8218bbed2126ee776fac5e0867f384038fe1f0bb
marwa-my <engineer.marwatalaat@gmail.com>
2025-03-04 23:21:10 +0200

    t3lim

--- app/src/main/java/com/salim/ta3limes/Activities/StartActivity.java
+++ app/src/main/java/com/salim/ta3limes/Activities/StartActivity.java
@@ -37,7 +37,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import me.anwarshahriar.calligrapher.Calligrapher;
